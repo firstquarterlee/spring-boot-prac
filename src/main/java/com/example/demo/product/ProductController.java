8ddf24cfb02ab87fe3155709db84c39f0ad29a41
firstquarterlee <firstquarterlee@gmail.com>
2024-04-05 03:08:17 +0900

    findProduct() 메소드 추가

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -1,4 +1,21 @@
 package com.example.demo.product;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class ProductController {
+    // "사용자(화면) 요청을 던지면" 그걸 받아서
+    // 요청에 맞는 메소드를 실행 시킵니다.
+
+    // 그 안에서, 그에 맞는 로직을 수행할 수 있도록 서비스한테 시킬거에요.
+
+    // 상품 조회
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String findProduct() {
+        return "NoteBook"; // 리스폰스 바디 데이터를 담아줘야함!
+    }
 }
